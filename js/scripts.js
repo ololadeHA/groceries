8bb87b906f2648b0482023b8198da2793aa50cb6
Arowolo Haleem <arowolohaleem@gmail.com>
2024-01-15 11:42:48 +0100

    add submit button

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+  $("#formOne").submit(function(event) {
+    const item1Input = $("input#item1").val();
+    const item2Input = $("input#item2").val();
+    const item3Input = $("input#item3").val();
+  })
+})
